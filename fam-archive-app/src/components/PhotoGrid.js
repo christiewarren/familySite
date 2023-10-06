ee064b45197acaf0f93ad264a5dbb08c238dc70d
Christie <christie@macbook.local>
2023-10-06 15:33:36 -0700

    added new photos and temporarily excluding ewarren_child

--- fam-archive-app/src/components/PhotoGrid.js
+++ fam-archive-app/src/components/PhotoGrid.js
@@ -6,12 +6,12 @@ export default function PhotoGrid(){
     const cards = images.map(image => {
         // {console.log(image)}
         return(
-            <div className={'card ' + image.orientation}>
+            image.filename != 'ewarren_child.jpeg' && <div className={'card ' + image.orientation}>
                 <Thumbnail
                 src={image.filename}
                 key={image.title}
                 />
-                {image.title && <div className='doc-title-bar'><p>{image.title}</p></div>}
+                {image.type == "document" && <div className='doc-title-bar'><p>{image.title}</p></div>}
             </div>
         )
       })
