590db2182d7fd5c0e95b34e568444f74dc62479a
Christie <christie@macbook.local>
2025-04-01 22:37:35 -0700

    test commit post-filter

--- fam-archive-app/src/components/Home.js
+++ fam-archive-app/src/components/Home.js
@@ -5,7 +5,7 @@ export default function Menu(props){
         <div className="homepage-wrap">
             <div className="homepage-text-wrap">
                 <h2 className="hand-title">Grandma Lane's</h2>
-                <h1>Family archive</h1><br/>
+                <h1>test Family archive</h1><br/>
                 <h5>The Lane-Warren-Nissley-Newcomer clan has a rich archive to share. Photos, stories, letters, and other mementos have been saved and passed down for over 150 years. Compiled and cataloged by Grandma Jinny Lane, this material is now being digitized and published on this site for you to peruse. Have an artifact or story you want to share? Contact us and we will add it to the archive.</h5>
                 <p className="">We hope you enjoy!</p>
                 <p>Dave & Christie</p>
